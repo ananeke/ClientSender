cad1edb6d35f9ad63cad9cc9d81137a5274924a9
Dorota Przyjemska-Krusińska <ananeke@gmail.com>
2020-02-11 15:34:31 +0100

    Add project files.

---
+++ ClientSender/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientSender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            adrress.Text = IPAddress.Loopback.ToString();
+            int ports = System.Convert.ToInt16(port.Text);
+            ;
+
+            try
+            {
+                TcpClient clients = new TcpClient(adrress.Text, ports);
+                info.Items.Add("Wysyłam zapytanie...");
+            }
+
+            catch (Exception ex)
+            {
+                info.Items.Add("Błąd: Nie udało się nawiązać połączenia!");
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
